8c6a614721d96a4728d40c74a8a2bee4cda44492
igor <igorlanmaq@gmail.com>
2023-04-04 11:02:18 -0300

    body params

--- src/server.js
+++ src/server.js
@@ -1,21 +1,14 @@
 const express = require("express");
 
 const app = express();
+app.use(express.json())
 
-app.get("/message/:id/:user", (request, response) => {
+app.post("/users", (request, response) => {
+const {name, email, password} = request.body
 
-    const {id, user} = request.params
-response.send(`
-id da mensagem: ${id}.
-nome do  usuario: ${user}.
-`)
-})
 
-app.get("/users", (request, response) => {
-    const {page, limit} = request.query
     response.send(`
-    pagina: ${page}
-    mostrar: ${limit}
+  usuario: ${name}. - email: ${email}. - senha:${password}.
     `)
 })
 
