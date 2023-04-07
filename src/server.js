fabb56d3e230f13de8d49690614b463586f65fa9
igor <igorlanmaq@gmail.com>
2023-04-07 14:07:55 -0300

    AppError ok

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ app.use(routes)
 
 app.use((error, request, response, next) => {
 if (error instanceof AppError) {
-    return response.status(error.satusCode).json({
+    return response.status(error.statusCode).json({
         status: "error",
         message: error.message
     });
